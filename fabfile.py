7c6e8425f8c45897deb2e334dd207536a03264f5
bryan kennedy <bkennedy@smm.org>
2014-04-29 23:10:30 -0500

    Compute slope geotiffs
    Use gdal to turn slope information into color

--- fabfile.py
+++ fabfile.py
@@ -5,13 +5,20 @@ Recipes for GDAL Geo Tif conversion
 from fabric.api import local
 import glob
 
+
+def slope(source):
+    """Convert a GeoTif to a slope GeoTif"""
+
+    target = filename_flag(source, 'slope')
+    cmd = 'gdaldem slope %s %s' % (source, target)
+    local(cmd)
+    return target
+
+
 def hillshade(source):
     """Convert a GeoTif to a hillshade GeoTif"""
 
-    # Append an identifier to the filename
     target = filename_flag(source, 'hillshade')
-
-    # Use gdal to convert the file
     cmd = 'gdaldem hillshade -compute_edges -co compress=lzw %s %s' % (
         source, target)
     local(cmd)

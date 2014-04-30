b622df4fdded617e7d5ee0dfd114ca620712cadd
bryan kennedy <bkennedy@smm.org>
2014-04-29 23:04:10 -0500

    Helper function for handling files
    Put some description into file names after we process them.

--- fabfile.py
+++ fabfile.py
@@ -4,3 +4,16 @@ Recipes for GDAL Geo Tif conversion
 
 from fabric.api import local
 import glob
+
+def filename_flag(filename, flag):
+    filename_parts = filename.split('.')
+    output = ''
+    count = len(filename_parts)
+    for part in filename_parts:
+        if count == 1:
+            output = output + '-' + flag + '.'
+        count = count - 1
+
+        output = output + part
+
+    return str(output)
